d215313ce7e091656ae6cbb06c2b67c08cb14bb1
Your Name <you@example.com>
2022-02-24 09:10:06 +0800

    commit

--- src/components/roam/AutoSteering.vue
+++ src/components/roam/AutoSteering.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="steering">
-    <div
-      class="buttonWrapper"
-      v-for="(button, index) in buttonRander"
-      :key="index"
-    >
+    <div v-for="(button, index) in buttonRander" :key="index">
       <div v-if="button === ''"></div>
       <div style="position: relative" v-if="button != ''">
         <a-button
@@ -71,63 +67,58 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+@import "../var";
+
 .steering {
   width: 200px;
   height: 200px;
   position: absolute;
   left: 100px;
   bottom: 60px;
-  display: flex;
+  display: grid;
   align-items: center;
-  // background-color: fade(@highlight-color,50%);
+  background-color: transparentize($highlight-color, 0.5);
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
   justify-content: center;
   align-items: center;
   box-shadow: 0px 0px 15px #fff;
   border-radius: 50%;
   color: #000;
   border: none !important;
-  .buttonWrapper {
-    width: 200px;
-    height: 200px;
-    display: grid;
-    border-radius: 50%;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-columns: repeat(3, 1fr);
-    .commonBtn {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 3;
-    }
-    .steeringButton {
-      background-color: rgba(0, 0, 0, 0);
-      border: none;
-      overflow: hidden;
-    }
-    .active {
-      background-color: rgba(0, 0, 0, 0);
-      border: none;
-      overflow: hidden;
-      // :global {
-      //   .anticon {
-      //     svg {
-      //       height: 2em;
-      //       width: 2em;
-      //       color: #007acc;
-      //     }
-      //   }
-      // }
-    }
+  .commonBtn {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 3;
+  }
+  .steeringButton {
+    background-color: rgba(0, 0, 0, 0);
+    border: none;
+    overflow: hidden;
+  }
+  .active {
+    background-color: rgba(0, 0, 0, 0);
+    border: none;
+    overflow: hidden;
     ::v-deep {
       .anticon {
         svg {
           height: 2em;
           width: 2em;
-          color: #fff;
-          box-shadow: none;
+          color: #007acc;
         }
+      }
+    }
+  }
+  ::v-deep {
+    .anticon {
+      svg {
+        height: 2em;
+        width: 2em;
+        color: #fff;
+        box-shadow: none;
       }
     }
   }
